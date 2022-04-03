bcbd88672d7cdb0ee4a255747e25b295259aca36
ana.mfreitas <ana.mfreitas@stone.com.br>
2022-04-03 20:18:01 -0300

    Sign Up working!!!!!!!!!!

--- app/src/main/java/com/example/mynotes/ui/cadastro/CadastroFragment.kt
+++ app/src/main/java/com/example/mynotes/ui/cadastro/CadastroFragment.kt
@@ -1,28 +1,80 @@
 package com.example.mynotes.ui.cadastro
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.mynotes.R
 import com.example.mynotes.databinding.FragmentCadastroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class CadastroFragment : Fragment() {
 
     private var _binding: FragmentCadastroBinding? = null
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCadastroBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with (binding) {
+            buttonSignUp.setOnClickListener {
+                val email = binding.editTextEmail.text.toString()
+                val password = binding.editTextSenha.text.toString()
+                createAccount(email, password)
+
+            }
+        }
+
+        auth = Firebase.auth
+    }
+
+    private fun createAccount(email: String, password: String) {
+        if (!validateForm()) {
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+        val user = auth.currentUser
+        if (user != null) {
+            findNavController().navigate(R.id.logInFragment)
+        }
+    }
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = binding.editTextEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            binding.editTextEmail.error = "Required."
+            valid = false
+        } else {
+            binding.editTextEmail.error = null
+        }
+
+        val password = binding.editTextSenha.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            binding.editTextSenha.error = "Required."
+            valid = false
+        } else {
+            binding.editTextSenha.error = null
+        }
+
+        return valid
     }
 
     override fun onDestroyView() {
